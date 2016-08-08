210ed3885954ccf8eda9275313c32c63eda95826
Roberto Aldera <aldrob001@myuct.ac.za>
2016-08-08 11:43:44 +0200

    Add Callen Fisher's serial_terminal code

--- Aldera_INEMO/src/serial_terminal.h
+++ Aldera_INEMO/src/serial_terminal.h
@@ -1,20 +1,18 @@
 /*
- * SerialTerminal.h
+ * serial_terminal.h
  *
- *  Created on: Jul 4, 2012
- *      Author: James Gowans
- *
- *      Description:  Provides functionality for printf and console via UART
+ *  Created on: Sep 15, 2014
+ *      Author: Callen Fisher
  */
 
+#ifndef SERIAL_TERMINAL_H_
+#define SERIAL_TERMINAL_H_
 
-#ifndef SERIALTERMINAL_H_
-#define SERIALTERMINAL_H_
-
-#include "stm32f10x.h"
+#include "CRC.h"
+#include "hardwareSetup.h"
+#include "stm32f4xx.h"
 #include "string.h"
-#define TX_BUFFER_SIZE 50
-
+#include "vComsTask.h"
 
 typedef struct {
   uint8_t data[TX_BUFFER_SIZE - 2];
@@ -22,18 +20,19 @@ typedef struct {
   uint16_t bytes_to_tx;
 } CommsTask_TransmitPacketStruct;
 
-uint8_t dataS1[TX_BUFFER_SIZE];
-uint8_t dataS2[TX_BUFFER_SIZE];
-uint8_t dataS3[TX_BUFFER_SIZE];
-uint8_t dataS4[TX_BUFFER_SIZE];
-uint16_t Tx_chars;
-uint8_t TxBuff[TX_BUFFER_SIZE];
+//FLAGS FOR SUCCESSFUL PACKETS
+uint8_t flagSuccessS1;
+uint8_t flagSuccessS2;
+uint8_t flagSuccessS3;
+
+//DATA RECEIVED LENGTHS
 uint16_t lengthS1;
+uint16_t lengthS2;
+uint16_t lengthS3;
+uint16_t lengthS4;
 
 void serialTerminal_Init();
-
-void serialTerminal_packetize(uint8_t* payload_to_packS1,uint8_t* payload_to_packS2,uint8_t* payload_to_packS3,uint8_t* payload_to_packS4, uint16_t length_of_payloadS1,uint16_t length_of_payloadS2,uint16_t length_of_payloadS3,uint16_t length_of_payloadS4);
-
+void serialTerminal_packetize(uint8_t* payload_to_packS1,uint8_t* payload_to_packS2,uint8_t* payload_to_packS3,uint8_t* payload_to_packS4, uint16_t length_of_payloadS1, uint16_t length_of_payloadS2, uint16_t length_of_payloadS3, uint16_t length_of_payloadS4,uint8_t success);
 void SerialTerminal_BeginTx(void);
 
-#endif /* SERIALTERMINAL_H_ */
+#endif /* SERIAL_TERMINAL_H_ */
